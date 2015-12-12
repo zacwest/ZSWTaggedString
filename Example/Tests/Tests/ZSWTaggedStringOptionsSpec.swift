37c887d3b4efcea15f245f21ae2cb17609fae755
Zachary West <zacwest@gmail.com>
2015-12-12 14:04:10 -0800

    Empty options spec

---
+++ Example/Tests/Tests/ZSWTaggedStringOptionsSpec.swift
@@ -0,0 +1,15 @@
+//
+//  ZSWTaggedStringOptionsSpec.swift
+//  ZSWTaggedString
+//
+//  Created by Zachary West on 12/12/15.
+//  Copyright © 2015 Zachary West. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import ZSWTaggedString
+
+class ZSWTaggedStringOptionsSpec: QuickSpec { override func spec() {
+    
+} }
