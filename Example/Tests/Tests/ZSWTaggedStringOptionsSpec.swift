cf258966f300d5956d596423001f6a2bb7bf61b5
Zachary West <zacwest@gmail.com>
2015-12-12 14:13:00 -0800

    Test setting/getting

--- Example/Tests/Tests/ZSWTaggedStringOptionsSpec.swift
+++ Example/Tests/Tests/ZSWTaggedStringOptionsSpec.swift
@@ -11,5 +11,50 @@ import Nimble
 @testable import ZSWTaggedString
 
 class ZSWTaggedStringOptionsSpec: QuickSpec { override func spec() {
-    
+    describe("swift setters") {
+        var options: ZSWTaggedStringOptions!
+        
+        beforeEach {
+            options = ZSWTaggedStringOptions()
+        }
+        
+        it("should allow setting a static attributes") {
+            options["a"] = .Static([
+                "key": true
+            ])
+            
+            guard let a = options["a"] else {
+                fail("Could not retrieve after setting")
+                return
+            }
+            
+            switch a {
+            case .Static(let attributes):
+                expect(attributes["key"] as? Bool) == true
+            case .Dynamic(_):
+                fail("Retrieved a dynamic when expecting static")
+            }
+        }
+        
+        it("should allow setting dynamic attributes") {
+            options["a"] = .Dynamic({ _, _, _ in
+                return [
+                    "key": true
+                ]
+            })
+            
+            guard let a = options["a"] else {
+                fail("Could not retrieve after setting")
+                return
+            }
+            
+            switch a {
+            case .Static(_):
+                fail("Retrieved a static when expecting dynamic")
+            case .Dynamic(let block):
+                let attributes = block("a", [String: AnyObject](), [String: AnyObject]())
+                expect(attributes["key"] as? Bool) == true
+            }
+        }
+    }
 } }
